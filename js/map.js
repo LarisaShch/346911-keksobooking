fae8ccc1962a71e14b3783c6d14b5959391f411a
Лариса Щербатых <coolprogrammer@mail.ru>
2018-12-08 22:05:49 +0300

    drag-n-drop

--- js/map.js
+++ js/map.js
@@ -51,6 +51,7 @@ var formSpace = form.querySelectorAll('fieldset');
 var filtersForm = document.querySelector('.map__filters');
 var filterSelector = filtersForm.querySelectorAll('.map__filter');
 var filterFeatures = filtersForm.querySelector('fieldset');
+var mainPin = document.querySelector('.map__pin--main');
 
 
 var getRandomNumber = function (min, max) {
@@ -273,12 +274,6 @@ pinButton.addEventListener('click', function (evt) {
 disableElements(formSpace);
 disableElements(filterSelector);
 
-// Заполнение  адреса
-
-var addres = document.querySelector('#address');
-var buttonX = parseInt(pinButton.style.left.replace('px', ''), 10) + 32;
-var buttonY = parseInt(pinButton.style.top.replace('px', ''), 10) + 84;
-addres.value = buttonX + ', ' + buttonY;
 
 // Проверка количества комнат и количества гостей
 
@@ -303,6 +298,85 @@ roomCapacity.addEventListener('change', function () {
   checkRoom();
 });
 
+
+// drag-n-drop
+var mainPinWidth = mainPin.offsetWidth;
+var mainPinStartHeight = mainPin.offsetHeight;
+var mainPinHeight = mainPinStartHeight + 22;
+var adress = document.querySelector('#address');
+var mapWidth = map.offsetWidth;
+
+var pinsLimits = {
+  MIN_Y: 130 - mainPinHeight,
+  MAX_Y: 630,
+  MIN_X: 0 - mainPinWidth / 2,
+  MAX_X: mapWidth - mainPinWidth / 2
+};
+
+var getLimit = function (value, min, max) {
+  if (value < min) {
+    value = min;
+  }
+
+  if (value > max) {
+    value = max;
+  }
+
+  return value;
+};
+var pinCoords = function (coords) {
+  coords.x = getLimit(coords.x, pinsLimits.MIN_X, pinsLimits.MAX_X);
+  coords.y = getLimit(coords.y, pinsLimits.MIN_Y, pinsLimits.MAX_Y);
+
+  return coords;
+};
+
+mainPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoord = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var coord = {
+      x: startCoord.x - moveEvt.clientX,
+      y: startCoord.y - moveEvt.clientY
+    };
+
+    var resultCoord = {
+      x: mainPin.offsetLeft - coord.x,
+      y: mainPin.offsetTop - coord.y
+    };
+
+    startCoord = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    pinCoords(resultCoord);
+
+    mainPin.style.top = resultCoord.y + 'px';
+    mainPin.style.left = resultCoord.x + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    var mainPinY = Math.ceil(mainPin.offsetTop + mainPinHeight);
+    var mainPinX = Math.ceil(mainPin.offsetLeft + mainPinWidth / 2);
+    adress.setAttribute('value', mainPinX + ' , ' + mainPinY);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+  mainPin.addEventListener('mouseup', pinButton);
+});
 
 generateArray(OBJ_QUANTITY);
 generatePin(objList);
